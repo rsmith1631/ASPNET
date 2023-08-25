7601fdd0e4914057509feb6885fa8943458dae55
rsmith1631 <rsmith1631@gmail.com>
2023-08-25 13:40:13 -0700

    Finished Create Product

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -9,5 +9,8 @@ namespace Testing.Models
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
         public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
     }
 }
--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Testing.Models
 {
@@ -11,5 +12,6 @@ namespace Testing.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
