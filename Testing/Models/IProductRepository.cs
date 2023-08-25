d4e0c62d61d0bb34f7dda3db48591a9dd1e7a10f
rsmith1631 <rsmith1631@gmail.com>
2023-08-24 21:17:47 -0700

    Added ASP.Net and created a product class

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
